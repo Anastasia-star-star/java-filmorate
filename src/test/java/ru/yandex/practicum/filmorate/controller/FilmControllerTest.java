0348cc4a70b08d193c42f48a6320cca42253ce8b
Anastasiia <aiarkhipova@yandex.ru>
2024-03-03 23:57:33 +0300

    fix4: codeStyle

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -19,21 +19,21 @@ class FilmControllerTest {
 
     @Test
     void validateFail() throws ValidationException {
-        final Film filmFail = new Film(1, " ", "Мелодрамма", LocalDateTime.now().minusYears(10), 58);
+        final Film filmFail = new Film(1, "", "Мелодрамма", LocalDateTime.now().minusYears(10), 58);
         Exception exception = assertThrows(ValidationException.class, () -> filmController.validate(filmFail));
-        assertEquals(exception, "Поле с названием фильма пустое");
+        assertEquals(exception.getMessage(), "Поле с названием фильма пустое");
 
         final Film filmFail2 = new Film(2, "Завтрак у Тифани", "Мелодраммаsdfghjkljhgfdfghjkljhgfdsfghjjhgfdfvgbnhmjjmhngbfjhgfh" + "fdghjkl;';lkjhgfdfghjklkjhgfdghjklkjhgfdghjkjhgfdgbhnmjhgfgbnhgnh" + "dfgbnhmjk,l./;tfghjkljhgfdsfghjkjhgfdsfghjkhgfdsfghjkjhgfdfghjkgf" + "fdghjkl;';lkjhgfdfghjklkjhgfdghjklkjhgfdghjkjhgfdgbhnmjhgfgbnhgnh" + "dfgbnhmjk,l./;tfghjkljhgfdsfghjkjhgfdsfghjkhgfdsfghjkjhgfdfghjkgf" + "fdghjkl;';lkjhgfdfghjklkjhgfdghjklkjhgfdghjkjhgfdgbhnmjhgfgbnhgnh" + "dfgbnhmjk,l./;tfghjkljhgfdsfghjkjhgfdsfghjkhgfdsfghjkjhgfdfghjkgf" + "fdghjklkjhgvdfghjkjhgfdsdfghjkjhgfdsasdfghjklkjhgfdsd" + "fghjkjhgfdasdfg", LocalDateTime.now().minusYears(10), 58);
         Exception exception2 = assertThrows(ValidationException.class, () -> filmController.validate(filmFail2));
-        assertEquals(exception2, "Поле с описанием фильма превышает 200 символов");
+        assertEquals(exception2.getMessage(), "Поле с описанием фильма превышает 200 символов");
 
         final Film filmFail3 = new Film(3, "Breakfast by Tifany ", "Мелодрамма", LocalDateTime.now().minusYears(200), 58);
         Exception exception3 = assertThrows(ValidationException.class, () -> filmController.validate(filmFail3));
-        assertEquals(exception3, "Дата реализации фильма раньше допустимой даты");
+        assertEquals(exception3.getMessage(), "Дата реализации фильма раньше допустимой даты");
 
         final Film filmFail4 = new Film(4, "Breakfast by Tifany ", "Мелодрамма", LocalDateTime.now().minusYears(10), -2);
         Exception exception4 = assertThrows(ValidationException.class, () -> filmController.validate(filmFail4));
-        assertEquals(exception4, "Длительность фильма отрицательна");
+        assertEquals(exception4.getMessage(), "Длительность фильма отрицательна");
 
     }
 }
