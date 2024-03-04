d652b8fb8d77da89d8c0094f4fb9c046f0ff3f2a
Anastasiia <aiarkhipova@yandex.ru>
2024-03-04 13:57:31 +0300

    fix7: build

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,14 +1,18 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Data;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-    private final int id;
-    private final String email;
-    private final String login;
+    private int id;
+    private String email;
+    private String login;
     private String name;
-    private final LocalDateTime birthday;
+    private LocalDateTime birthday;
 }
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -13,34 +13,33 @@ class UserControllerTest {
 
     @Test
     void validateOK() throws ValidationException {
-        final User userOk = new User(0, "aiarkhipova@yandex.ru", "la-la",
+        final User userOk = new User(0, "aiarkhipova@yandex.ru", "la-la", "Anastasia",
                 LocalDateTime.now().minusYears(22));
-        userOk.setName("Anastasia");
         assertTrue(userController.validate(userOk));
     }
 
     void validateFail() {
-        final User userFail = new User(1, "aiarkhipova@yandex.ru", "",
+        final User userFail = new User(1, "aiarkhipova@yandex.ru", "", "Anastasia",
                 LocalDateTime.now().minusYears(22));
         Exception exception = assertThrows(ValidationException.class, () -> userController.validate(userFail));
         assertEquals(exception.getMessage(), "Пустое поле с логином");
 
-        final User userFail2 = new User(2, "aiarkhipova@yandex.ru", "la la",
+        final User userFail2 = new User(2, "aiarkhipova@yandex.ru", "la la", "Anastasia",
                 LocalDateTime.now().minusYears(22));
         Exception exception2 = assertThrows(ValidationException.class, () -> userController.validate(userFail2));
         assertEquals(exception2.getMessage(), "В логине присутствуют пробелы");
 
-        final User userFail3 = new User(3, "aiarkhipovayandex.ru", "",
+        final User userFail3 = new User(3, "aiarkhipovayandex.ru", "", "Anastasia",
                 LocalDateTime.now().minusYears(22));
         Exception exception3 = assertThrows(ValidationException.class, () -> userController.validate(userFail3));
         assertEquals(exception3.getMessage(), "В электронной почте нет символа собачки");
 
-        final User userFail4 = new User(4, "", "",
+        final User userFail4 = new User(4, "", "", "Anastasia",
                 LocalDateTime.now().minusYears(22));
         Exception exception4 = assertThrows(ValidationException.class, () -> userController.validate(userFail4));
         assertEquals(exception4.getMessage(), "Пустое поле с электронной почтой");
 
-        final User userFail5 = new User(5, "aiarkhipova@yandex.ru", "",
+        final User userFail5 = new User(5, "aiarkhipova@yandex.ru", "", "Anastasia",
                 LocalDateTime.now().plusYears(50));
         Exception exception5 = assertThrows(ValidationException.class, () -> userController.validate(userFail5));
         assertEquals(exception5.getMessage(), "День рождения пользователя в будущем");
